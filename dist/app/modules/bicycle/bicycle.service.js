c774ca31b3a631620b4ee66dab0597597ffd39f9
asif419 <asshah419@gmail.com>
2024-11-24 17:37:48 +0200

    trying to deploy

---
+++ dist/app/modules/bicycle/bicycle.service.js
@@ -0,0 +1,46 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.bicycleServices = void 0;
+const bicycle_model_1 = require("./bicycle.model");
+const createBicycleIntoDB = (bicycleData) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield bicycle_model_1.Bicycle.create(bicycleData);
+    return result;
+});
+const searchBicyclesFromDB = (term) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield bicycle_model_1.Bicycle.find({
+        $or: [
+            { name: { $eq: term } },
+            { brand: { $eq: term } },
+            { type: { $eq: term } },
+        ],
+    });
+    return result;
+});
+const getBicycleByID = (productID) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield bicycle_model_1.Bicycle.findById(productID);
+    return result;
+});
+const updateBicycleByID = (productID, bicycleData) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield bicycle_model_1.Bicycle.findOneAndUpdate({ _id: { $eq: productID } }, { $set: bicycleData }, { new: true });
+    return result;
+});
+const deleteBicycleFromDB = (productID) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield bicycle_model_1.Bicycle.updateOne({ _id: { $eq: productID }, isDeleted: { $ne: true } }, { isDeleted: true });
+    return result;
+});
+exports.bicycleServices = {
+    createBicycleIntoDB,
+    searchBicyclesFromDB,
+    getBicycleByID,
+    updateBicycleByID,
+    deleteBicycleFromDB,
+};
---
+++ dist/app/modules/order/order.controller.js
@@ -0,0 +1,78 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.orderControllers = void 0;
+const order_validation_1 = __importDefault(require("./order.validation"));
+const order_services_1 = require("./order.services");
+const createOrder = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    let setResponse = false;
+    try {
+        const orderData = req.body;
+        // zod validation
+        const zodPerseOrder = order_validation_1.default.parse(orderData);
+        const result = yield order_services_1.orderServices.createOrderIntoDB(zodPerseOrder);
+        res.status(200).json({
+            message: 'Order created successfully',
+            success: true,
+            data: result,
+        });
+    }
+    catch (err) {
+        if (err.message === 'Bicycle not Found.' && !setResponse) {
+            res.status(404).json({
+                success: false,
+                message: 'Bicycle not found in the inventory.',
+            });
+            setResponse = true;
+        }
+        else if (err.message === 'Insufficient stock for the bicycle.' &&
+            !setResponse) {
+            res.status(400).json({
+                success: false,
+                message: 'Insufficient stock to fulfill the order.',
+            });
+            setResponse = true;
+        }
+        else if (setResponse) {
+            res.status(500).json({
+                success: false,
+                message: err.message || 'Something went wrong',
+                error: err,
+            });
+        }
+    }
+});
+const getRevenue = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const result = yield order_services_1.orderServices.getRevenueFromDB();
+        res.status(200).json({
+            message: 'Revenue calculated successfully',
+            success: true,
+            data: {
+                result,
+            },
+        });
+    }
+    catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message || 'Something went wrong',
+            error: err,
+        });
+    }
+});
+exports.orderControllers = {
+    createOrder,
+    getRevenue,
+};
